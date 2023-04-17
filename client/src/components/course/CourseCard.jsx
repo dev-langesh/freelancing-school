6daf3d43ca19e24f47d55f9d940cde8a33b5db6b
langesh <langesh@gmail.com>
2023-04-17 16:46:43 +0530

    course card

--- client/src/components/course/CourseCard.jsx
+++ client/src/components/course/CourseCard.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 export default function CourseCard({ num, code, name, description }) {
   return (
-    <div className="w-full shadow-xl shadow-black border-l-4 border-blue-400 p-4">
+    <section className="w-full shadow-xl shadow-black border-l-4 border-blue-400 p-4">
       <h1 className="text-blue-500 font-bold">{name}</h1>
 
       <p className="text-slate-400 text-sm">{description}</p>
-    </div>
+
+      <div className="flex items-center gap-10 pt-4">
+        <p className="text-slate-500 font-bold text-sm">
+          Course code: <span className="text-slate-300">{code}</span>
+        </p>
+        <p className="text-slate-500 font-bold text-sm">
+          Course number: <span className="text-slate-300">{num}</span>
+        </p>
+      </div>
+    </section>
   );
 }
